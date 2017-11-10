c736bcff4eede837fc0cba8e6014ffc4536e35a6
sqmwin <sqmwin@Hotmail.com>
2017-11-10 15:37:45 +0800

    有关web应用中servlet的响应(response)的测试

---
+++ src/login/LoginServlet.java
@@ -0,0 +1,48 @@
+package login;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 登陆失败重定向
+ * 若login成功,则访问此sevlet,若login失败,则重定向回登陆页面
+ * @author sqm
+ * @Date 2017-11-10
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = "/loginservlet")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        //通过request获得提交表单中的用户名和密码
+        //getParameter(String)--通过表单中的name属性获取表单中的参数
+        String name = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        //如果name和password都相同则登陆成功，如果name和password不同则重定向页面
+        if ("admin".equals(name) && "admin".equals(password)) {
+            //登陆成功，响应
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().write("登陆成功");
+
+        }else {
+            //用户名或密码错误，则重定向，返回登陆页面
+            response.setStatus(302);
+            response.setHeader("Location", "/servlet/html/login.html");
+            response.sendRedirect("/servlet/html/login.html");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //doGet方法和doPost方法相同时使用
+        this.doGet(request, response);
+    }
+
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,13 @@
+<%-- Created by IntelliJ IDEA. --%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>ResponseAndRequset</title>
+    <link rel="stylesheet" type="text/css" href="./css/cascadingstylesheet.css">
+  </head>
+  <body>
+  <div class="div">
+    <a href="./html/login.html">loginTest</a>
+  </div>
+  </body>
+</html>
